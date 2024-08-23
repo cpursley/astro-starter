1bbf46ff5d5d201ba8e4a472ad5ebad58c0f0182
Chase Pursley <chasepursley@gmail.com>
2024-08-23 15:37:06 -0400

    Sitemap take 2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://findanappraiser.com",
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
