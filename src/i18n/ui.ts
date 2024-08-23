e8534e5dcf80a0a08eb995ce59819f041d92e8df
Chase Pursley <chasepursley@gmail.com>
2024-08-23 18:56:21 -0400

    Set up i18n for config - take 2.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,23 +3,23 @@ export const defaultLang = 'en' as const;
 export const ui = {
   'en': {
     // Metadata
-    'siteMetadata.title': 'Website',
+    'siteMetadata.title': 'Astro Starter',
     'siteMetadata.url': 'website.com',
 
     // Home
-    'pages.home.title': `{title}`,
+    'pages.home.title': "Home",
     'pages.home.description': "This is the the homepage",
 
     // About
-    'pages.about.title': `About {title}`,
+    'pages.about.title': "About",
     'pages.about.description': "About this site.",
 
     // Blog
-    'pages.blog.title': `{title} Blog`,
+    'pages.blog.title': `Blog`,
     'pages.blog.description': "This is the blog",
 
     // Tags
-    'pages.tags.title': `{title} Blog Post Tags`,
+    'pages.tags.title': `Blog Post Tags`,
     'pages.tags.description': "This are the blog post tags",
   }
 };
