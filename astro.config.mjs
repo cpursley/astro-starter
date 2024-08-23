46ce0eec03d66235f7b2eb28cf910fb1dc8166c4
Chase Pursley <chasepursley@gmail.com>
2024-08-23 17:06:02 -0400

    Set up SEO stuff.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://findanappraiser.com",
+  site: "https://website.com",
   integrations: [tailwind(), sitemap(), robotsTxt()]
 });
