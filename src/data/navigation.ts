73d43b1e3456e80840d40e42796ff7ab1e083971
Chase Pursley <chasepursley@gmail.com>
2024-08-27 10:25:42 -0400

    Add Services page.

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -6,5 +6,6 @@ interface NavItem {
 export const navItems: NavItem[] = [
     { href: "/", text: "Home" },
     { href: "/about", text: "About" },
+    { href: "/services", text: "Services" },
     { href: "/blog", text: "Blog" },
 ];
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,26 +1,34 @@
-export const defaultLang = 'en' as const;
+export const defaultLang = "en" as const;
 
 export const ui = {
-  'en': {
+  "en": {
     // Metadata
-    'siteMetadata.language': 'en-US',
-    'siteMetadata.title': 'Astro Starter',
-    'siteMetadata.url': 'website.com',
+    "siteMetadata.language": "en-US",
+    "siteMetadata.title": "Astro Starter",
+    "siteMetadata.url": "website.com",
 
     // Home
-    'pages.home.title': "Home",
-    'pages.home.description': "This is the homepage",
+    "pages.home.title": "Home",
+    "pages.home.subtitle": "Our Home",
+    "pages.home.description": "This is the homepage",
 
     // About
-    'pages.about.title': "About",
-    'pages.about.description': "About this site.",
+    "pages.about.title": "About",
+    "pages.about.subtitle": "Our Team",
+    "pages.about.description": "About this site.",
+
+    // Services
+    "pages.services.title": "Our Services",
+    "pages.services.subtitle": "What we offer",
+    "pages.services.description": "This are our services",
 
     // Blog
-    'pages.blog.title': `Blog`,
-    'pages.blog.description': "This is the blog",
+    "pages.blog.title": "Blog",
+    "pages.blog.subtitle": "Our blog",
+    "pages.blog.description": "This is the blog",
 
     // Tags
-    'pages.tags.title': `Blog Post Tags`,
-    'pages.tags.description': "This are the blog post tags",
+    "pages.tags.title": `Blog Post Tags`,
+    "pages.tags.description": "This are the blog post tags",
   }
 };
